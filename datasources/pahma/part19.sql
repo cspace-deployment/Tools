5a25151a9d2a55a676c165adf4b8ee164b41bb56
Michael Black <mtblack@berkeley.edu>
2016-04-14 15:32:46 -0700

    PAHMA-1491: removed condition from select and removed unnecessary join to collectionobjects_naturalhistory

--- datasources/pahma/part19.sql
+++ datasources/pahma/part19.sql
@@ -2,8 +2,7 @@ SELECT
   cc.id,
   regexp_replace(cc.computedcurrentlocation, '^.*\)''(.*)''$', '\1')         AS currentlocation_s,
   regexp_replace(ca.computedcrate, '^.*\)''(.*)''$', '\1')                   AS computedcrate_s,
-  STRING_AGG(DISTINCT REGEXP_REPLACE(adg.donor, '^.*\)''(.*)''$', '\1'),'␥') AS "donor_ss",
-  'condition'                                                                AS condition_s
+  STRING_AGG(DISTINCT REGEXP_REPLACE(adg.donor, '^.*\)''(.*)''$', '\1'),'␥') AS "donor_ss"
 
 FROM collectionobjects_common cc
   JOIN hierarchy h1 ON (h1.id=cc.id)
@@ -13,7 +12,6 @@ FROM collectionobjects_common cc
   LEFT OUTER JOIN acquisitions_common ac ON (h2.id=ac.id)
   LEFT OUTER JOIN hierarchy h3 ON (ac.id=h3.parentid AND h3.name='acquisitions_pahma:acquisitionDonorGroupList')
   LEFT OUTER JOIN acquisitiondonorgroup adg ON (adg.id=h3.id)
-  FULL OUTER JOIN collectionobjects_naturalhistory conh ON (cc.id=conh.id)
   JOIN misc ON (cc.id = misc.id AND misc.lifecyclestate <> 'deleted')
   
  GROUP BY cc.id, cc.computedcurrentlocation, ca.computedcrate
