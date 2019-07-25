5a139ae6ddaa98618ea54228557ed3040d772fe3
John Lowe <jblowe@berkeley.edu>
2019-07-25 05:22:04 -0700

    CSW-28: update the Solr test script to Pyhon3, etc.

--- datasources/utilities/tS.py
+++ datasources/utilities/tS.py
@@ -3,26 +3,26 @@ import sys
 
 try:
     core = sys.argv[1]
+    host = sys.argv[2]
 except:
-    print "syntax: python %s solar_core" % sys.argv[0]
-    print "e.g     python %s pahma-public" % sys.argv[0]
+    print("syntax: python %s solar_core hostname" % sys.argv[0])
+    print("e.g     python %s pahma-public https://webapps.cspace.berkeley.edu" % sys.argv[0])
     sys.exit(1)
 
 try:
     # create a connection to a solr server
-    s = solr.SolrConnection(url = 'http://localhost:8983/solr/%s' % core, http_user = 'guest', http_pass = '')
+    s = solr.SolrConnection(url = '%s/solr/%s' % (host,core))
 
     # do a search
     response = s.query('*:*', rows=20)
-    print '%s, records found: %s' % (core,response._numFound)
+    print('%s, records found: %s' % (core,response._numFound))
 
     details = False
 
     if details:
         for hit in response.results:
             for h in hit:
-                print hit[h],
-            print
+                print(hit[h])
 except:
-    print "could not access %s." % core
+    print("could not access %s." % core)
 
