0059fc432b8db020d8ebd3efcce09915a485c698
John Lowe <jblowe@berkeley.edu>
2019-07-26 15:46:58 -0700

    CSW-28: evaluate.py seems to need an 'escapechar' set in some cases

--- datasources/ucjeps/evaluate.py
+++ datasources/ucjeps/evaluate.py
@@ -21,8 +21,8 @@ types = {}
 errors = 0
 
 with open(sys.argv[2], 'w') as f2:
-   writer = csv.writer(f2, delimiter=delim, quoting=csv.QUOTE_NONE, quotechar=chr(255))
-   with open(sys.argv[1], 'r') as f1:
+    writer = csv.writer(f2, delimiter=delim, quoting=csv.QUOTE_NONE, quotechar=chr(255), escapechar='\\')
+    with open(sys.argv[1], 'r') as f1:
         reader = csv.reader(f1, delimiter=delim, quoting=csv.QUOTE_NONE, quotechar=chr(255))
         for lineno, row in enumerate(reader):
             if lineno == 0:
