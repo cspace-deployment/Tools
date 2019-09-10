322424fde5bd7b107eaa83b10ea1ede8df07d24f
Lam Voong <lkv@berkeley.edu>
2019-09-10 16:59:30 -0700

    CC-759: Fix concat error

--- functions/alldeployments/get_uoc_authby.sql
+++ functions/alldeployments/get_uoc_authby.sql
@@ -9,8 +9,8 @@ DECLARE authstr VARCHAR(4000);
 
 BEGIN
 
-select '&nbsp;&nbsp;&nbsp;&nbsp;' ||string_agg(
-        case when ag.authorizationdate is null then '' else '' || to_char(ag.authorizationdate, 'yyyy-mm-dd') || ': ' end
+select '&nbsp;&nbsp;&nbsp;&nbsp;' || string_agg(
+        case when ag.authorizationdate is null then '' else to_char(ag.authorizationdate, 'yyyy-mm-dd') || ': ' end
         || case when ag.authorizationstatus is null then '' else '<b>' || getdispl(ag.authorizationstatus) || '</b>: ' end
         || getdispl(ag.authorizedby), '<br>&nbsp;&nbsp;&nbsp;&nbsp;' order by ag.authorizationdate)
 into authstr
