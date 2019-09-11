9ccb63af874363e2e2a2fe0fae6c5865a5ddb2a3
Lam Voong <lkv@berkeley.edu>
2019-09-11 15:27:41 -0700

    CC-814: Add code for getdispl() for all deployments

---
+++ functions/alldeployments/getdispl.sql
@@ -0,0 +1,13 @@
+-- DROP FUNCTION getdispl(text)
+
+CREATE OR REPLACE FUNCTION getdispl(text)
+    RETURNS text
+    LANGUAGE sql
+    IMMUTABLE STRICT
+AS $function$
+    SELECT regexp_replace($1, '^.*\)''(.*)''$', '\1')
+$function$
+;
+
+GRANT EXECUTE ON FUNCTION getdispl(text) to public;
+
