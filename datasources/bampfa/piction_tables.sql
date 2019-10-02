264bbd371b9eb22559c0ddc2e9fe6b3c1e0d0219
Lam Voong <lkv@berkeley.edu>
2019-10-02 09:50:25 -0700

    CC-859: Add object_number column and indexes to piction tables

--- datasources/bampfa/piction_tables.sql
+++ datasources/bampfa/piction_tables.sql
@@ -1,4 +1,11 @@
+/*
 -- pg_dump unavailable, so the create statements for piction_interface and piction_history are re-engineered from table descriptions
+-- 2019/10/10: added object_number column, references collectionobjects_common.objectnumber
+
+-- DATABASE: piction_transit
+--   SCHEMA: piction
+--    TABLE: piction_interface
+*/
 
 CREATE TABLE piction_interface
 (
@@ -9,6 +16,7 @@ CREATE TABLE piction_interface
 	img_size integer,
 	img_height integer,
 	img_width integer,
+	object_number character varying,
 	object_csid character varying (100),
 	action character varying(20),
 	relationship character varying(20),
@@ -20,55 +28,47 @@ CREATE TABLE piction_interface
 	website_display_level character varying(30)
 );
 
--- ALTER TABLE piction_interface OWNER TO piction;
+CREATE INDEX piction_interface_piction_id_idx ON piction_interface (piction_id);
 
-CREATE INDEX idx_pictintf ON piction_interface (piction_id);
+GRANT ALL ON piction_interface TO piction;
+GRANT INSERT, SELECT, UPDATE, DELETE ON piction_interface TO piction_app_role;
+
+GRANT SELECT, UPDATE, USAGE ON piction_interface_id_seq TO piction;
+GRANT USAGE ON piction_interface_id_seq TO piction_app_role;
+
+-- DATABASE: piction_transit
+--   SCHEMA: piction
+--    TABLE: piction_history
+
+CREATE TABLE piction_history
+(
+	id bigint NOT NULL,
+	piction_id integer NOT NULL,
+	filename text NOT NULL,
+	mimetype character varying(100),
+	img_size integer,
+	img_height integer,
+	img_width integer,
+	object_number character varying,
+	object_csid character varying (100),
+	action character varying(20),
+	relationship character varying(20),
+	dt_addedtopiction timestamp without time zone,
+	dt_uploaded timestamp without time zone,
+	bimage bytea,
+	dt_processed timestamp without time zone,
+	sha1_hash character varying(40),
+	website_display_level character varying(30)
+);
+
+CREATE INDEX piction_history_object_number_idx ON piction_history (object_number);
+GRANT ALL ON piction_history TO piction;
+
+/* alternate method for creating piction_history table
 
 CREATE TABLE piction_history (LIKE piction_interface);
+CREATE INDEX piction_history_object_number_idx ON piction_history (object_number);
+GRANT ALL ON piction_history TO piction;
 
--- ALTER TABLE piction_history OWNER TO piction;
-
-/*
-                                          Table "piction.piction_interface"
-        Column         |            Type             |                           Modifiers                            
------------------------+-----------------------------+----------------------------------------------------------------
- id                    | bigint                      | not null default nextval('piction_interface_id_seq'::regclass)
- piction_id            | integer                     | not null
- filename              | text                        | not null
- mimetype              | character varying(100)      | 
- img_size              | integer                     | 
- img_height            | integer                     | 
- img_width             | integer                     | 
- object_csid           | character varying(100)      | 
- action                | character varying(20)       | 
- relationship          | character varying(20)       | 
- dt_addedtopiction     | timestamp without time zone | 
- dt_uploaded           | timestamp without time zone | 
- bimage                | bytea                       | 
- dt_processed          | timestamp without time zone | 
- sha1_hash             | character varying(40)       | 
- website_display_level | character varying(30)       | 
-Indexes:
-    "piction_interface_pkey" PRIMARY KEY, btree (id)
-    "idx_pictintf" btree (piction_id)
-
-                 Table "piction.piction_history"
-        Column         |            Type             | Modifiers 
------------------------+-----------------------------+-----------
- id                    | bigint                      | not null
- piction_id            | integer                     | not null
- filename              | text                        | not null
- mimetype              | character varying(100)      | 
- img_size              | integer                     | 
- img_height            | integer                     | 
- img_width             | integer                     | 
- object_csid           | character varying(100)      | 
- action                | character varying(20)       | 
- relationship          | character varying(20)       | 
- dt_addedtopiction     | timestamp without time zone | 
- dt_uploaded           | timestamp without time zone | 
- bimage                | bytea                       | 
- dt_processed          | timestamp without time zone | 
- sha1_hash             | character varying(40)       | 
- website_display_level | character varying(30)       | 
 */
+
