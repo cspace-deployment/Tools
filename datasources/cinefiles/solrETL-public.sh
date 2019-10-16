3bb94118d79fcf02c7cdbfbbb672bdbb2efb4373
John Lowe <jblowe@berkeley.edu>
2019-10-16 07:58:03 -0700

    CSW-132: demo cinefiles-public solr core improvements

--- datasources/cinefiles/solrETL-public.sh
+++ datasources/cinefiles/solrETL-public.sh
@@ -19,7 +19,7 @@ SERVER="dba-postgres-prod-42.ist.berkeley.edu port=5313 sslmode=prefer"
 USERNAME="reporter_${TENANT}"
 DATABASE="${TENANT}_domain_${TENANT}"
 CONNECTSTRING="host=$SERVER dbname=$DATABASE"
-CONTACT="jblowe@berkeley.edu"
+CONTACT="cspace-support@lists.berkeley.edu"
 ##############################################################################
 # extract metadata and media info from CSpace
 ##############################################################################
@@ -42,7 +42,7 @@ perl -i -pe 's/\r//;s/^/d/;s/\t/_s\t/g;s/ddoc_id_s/id/;s/$/_s\tblob_ss/;s/_ss_s/
 time perl mergeObjectsAndMedia.pl media.csv docs.csv > d6.csv
 # we want to use our "special" solr-friendly header.
 tail -n +2 d6.csv | grep -v " rows)" > d7.csv
-cat header4Solr.csv d7.csv > docs.csv
+cat header4Solr.csv d7.csv > 4solr.${TENANT}.${CORE}.csv
 
 # make the header
 head -1 films.csv > header4Solr.csv
@@ -57,33 +57,32 @@ wc -l *.csv
 ##############################################################################
 # check if we have enough data to be worth refreshing...
 ##############################################################################
-for f in docs.csv films.csv; do
-  time python evaluate.py $f /dev/null > counts.$f &
-  CSVFILE=${f}
-  # this value is an approximate lower bound on the number of rows there should
-  # be, based on data as of 2019-09-11. It may need to be periodically adjusted.
-  MINIMUM=10000
-  ROWS=`wc -l < ${CSVFILE}`
-  if (( ${ROWS} < ${MINIMUM} )); then
-     echo "Only ${ROWS} rows in ${CSVFILE}; refresh aborted, core left untouched." | mail -s "PROBLEM with ${TENANT}-${CORE} nightly solr refresh" -- ${CONTACT}
-     exit 1
-  fi
-done;
+CSVFILE="4solr.${TENANT}.${CORE}.csv"
+# this value is an approximate lower bound on the number of rows there should
+# be, based on data as of 2019-09-11. It may need to be periodically adjusted.
+MINIMUM=50000
+ROWS=`wc -l < ${CSVFILE}`
+if (( ${ROWS} < ${MINIMUM} )); then
+   echo "Only ${ROWS} rows in ${CSVFILE}; refresh aborted, core left untouched." | mail -s "PROBLEM with ${TENANT}-${CORE} nightly solr refresh" -- cspace-support@lists.berkeley.edu
+   exit 1
+fi
 ##############################################################################
 # OK, we are good to go! clear out the existing data
 ##############################################################################
 curl -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update" --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
 curl -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update" --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 # note: we skip current location and current crate in loading Solr...
-time curl -X POST -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update/csv?commit=true&header=true&trim=true&separator=%09&f.grouptitle_ss.split=true&f.grouptitle_ss.separator=;&f.othernumbers_ss.split=true&f.othernumbers_ss.separator=;&f.blob_ss.split=true&f.blob_ss.separator=,&encapsulator=\\" -T docs.csv -H 'Content-type:text/plain; charset=utf-8' &
-time curl -X POST -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update/csv?commit=true&header=true&trim=true&separator=%09&f.grouptitle_ss.split=true&f.grouptitle_ss.separator=;&f.othernumbers_ss.split=true&f.othernumbers_ss.separator=;&f.blob_ss.split=true&f.blob_ss.separator=,&encapsulator=\\" -T films.csv -H 'Content-type:text/plain; charset=utf-8' &
+time curl -X POST -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update/csv?commit=true&header=true&trim=true&separator=%09&f.grouptitle_ss.split=true&f.grouptitle_ss.separator=;&f.othernumbers_ss.split=true&f.othernumbers_ss.separator=;&f.blob_ss.split=true&f.blob_ss.separator=,&encapsulator=\\" -T 4solr.${TENANT}.${CORE}.csv -H 'Content-type:text/plain; charset=utf-8' &
+# time curl -X POST -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update/csv?commit=true&header=true&trim=true&separator=%09&f.grouptitle_ss.split=true&f.grouptitle_ss.separator=;&f.othernumbers_ss.split=true&f.othernumbers_ss.separator=;&f.blob_ss.split=true&f.blob_ss.separator=,&encapsulator=\\" -T films.csv -H 'Content-type:text/plain; charset=utf-8' &
+time python3 evaluate.py 4solr.${TENANT}.${CORE}.csv /dev/null > counts.${CORE}.csv &
 # get rid of intermediate files
 # count blobs
-cut -f51 docs.csv | grep -v 'blob_ss' |perl -pe 's/\r//' |  grep . | wc -l > counts.${CORE}.blobs.csv
-cut -f51 docs.csv | perl -pe 's/\r//;s/,/\n/g;s/\|/\n/g;' | grep -v 'blob_ss' | grep . | wc -l >> counts.${CORE}.blobs.csv &
+cut -f51 4solr.${TENANT}.${CORE}.csv | grep -v 'blob_ss' |perl -pe 's/\r//' |  grep . | wc -l > counts.${CORE}.blobs.csv
+cut -f51 4solr.${TENANT}.${CORE}.csv | perl -pe 's/\r//;s/,/\n/g;s/\|/\n/g;' | grep -v 'blob_ss' | grep . | wc -l >> counts.${CORE}.blobs.csv &
 wait
 cp counts.${CORE}.blobs.csv /tmp/${TENANT}.counts.${CORE}.blobs.csv
-rm d?.csv m?.csv b?.csv media.csv docs.csv
+# rm d?.csv m?.csv b?.csv media.csv docs.csv films.csv header4Solr.csv
+rm d?.csv m?.csv b?.csv header4Solr.csv
 cat counts.${CORE}.blobs.csv
 # zip up .csvs, save a bit of space on backups
 gzip -f *.csv
