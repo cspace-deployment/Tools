0ed92f3be45e00599b9ec7daf517d13acee6efd6
John Lowe <jblowe@berkeley.edu>
2020-01-23 08:55:48 -0800

    NOJIRA: quiet the PAHMA batch_barcode cleanup script a bit

--- scripts/pahma/batch_barcode/clean_temp.sh
+++ scripts/pahma/batch_barcode/clean_temp.sh
@@ -7,4 +7,4 @@ find temp/location/done -type f  -delete
 find temp/location/done_id -type f  -delete
 find temp/relation/done -type f  -delete
 
-find /tmp/all_*  -type f -delete
+find /tmp/all_*  -type f -delete > /dev/null 2>&1
