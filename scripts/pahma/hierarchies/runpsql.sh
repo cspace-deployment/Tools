8b77afb2bf336f92c717c6d4b10350709201463b
John Lowe <jblowe@berkeley.edu>
2020-07-18 12:04:20 -0700

    CSW-345: pahma hierarchies scripts now get pw from .pgpass

--- scripts/pahma/hierarchies/runpsql.sh
+++ scripts/pahma/hierarchies/runpsql.sh
@@ -3,5 +3,5 @@ HOSTNAME="dba-postgres-prod-45.ist.berkeley.edu port=$2 sslmode=prefer"
 #HOSTNAME="dba-postgres-dev-45.ist.berkeley.edu port=$2 sslmode=prefer"
 USERNAME="nuxeo_${TENANT}"
 DATABASE="${TENANT}_domain_${TENANT}"
-CONNECTSTRING="host=$HOSTNAME dbname=$DATABASE password=xxxx"
+CONNECTSTRING="host=$HOSTNAME dbname=$DATABASE"
 psql -e -a -U $USERNAME -d "$CONNECTSTRING" -f $3
